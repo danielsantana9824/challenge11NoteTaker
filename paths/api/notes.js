7ab6eb492771049d1bbe4171401c53894850f508
danielsantana9824 <danielsantana9824@gmail.com>
2024-10-18 23:31:35 -0500

    update
    with this modification the user can add any task, each id is diferente and i modified the bd

--- paths/api/notes.js
+++ paths/api/notes.js
@@ -1,16 +1,65 @@
-const router=require("express").Router();
-const fs=require("fs");
+const router = require("express").Router();
+const fs = require("fs");
 
-router.get("/", (req,res)=>{
+router.get("/", (req, res) => {
 
-    fs.readFile("./db/db.json","utf-8", (err,data)=>{
-        if(err){
-           res.status(500).json(err)
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
+            res.status(500).json(err)
         }
-        const notes=JSON.parse(data)
+        const notes = JSON.parse(data)
         res.json(notes)
 
     })
 })
 
-module.exports=router;
+
+router.post('/', (req, res) => {
+
+    fs.readFile("./db/db.json", "utf-8", (err, data) => {
+        if (err) {
+            res.status(500).json(err)
+        }
+
+        let newUpdate = [];
+
+        newUpdate = JSON.parse(data)
+
+        const { title, text } = req.body;
+
+        if (title && text) {
+
+            const newTask = {
+                title,
+                text,
+                id: newUpdate.length + 1,
+            };
+
+            newUpdate.push(newTask);
+
+            const reviewString = JSON.stringify(newUpdate, null, 4);
+
+            fs.writeFile("./db/db.json", reviewString, (err) =>
+                err
+                    ? console.error(err)
+                    : console.log(
+                        `Review has been written to JSON file`
+                    )
+            );
+
+            const response = {
+                status: 'success',
+                body: reviewString,
+            };
+
+            console.log(response);
+            res.status(201).json(response);
+
+        }
+
+    })
+});
+
+
+
+module.exports = router;
